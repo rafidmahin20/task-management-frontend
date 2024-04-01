8baf0c9f08223e52eb58a4fbc710fe8b3a96b9b1
rafidmahin20 <submanmahin@gmail.com>
2024-04-02 00:33:00 +0600

    edited

--- task-management-client/src/App.jsx
+++ task-management-client/src/App.jsx
@@ -5,18 +5,64 @@ import Login from './Components/Login';
 import Register from './Components/Register';
 import Profile from './Components/Profile';
 import Header from './Components/Header';
+import { Toaster } from 'react-hot-toast';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
 const App = () => {
+  const [isVerified, setIsVerified] = useState(false);
+  const [tasks, setTasks] = useState([]);
+  const [user, setUser] = useState({});
+
+  useEffect(() => {
+    const handleGetUser = async () => {
+      try {
+        const {data} = await axios.get(
+          "http://localhost:4000/api/v1/user/profile",
+          {withCredentials: true}
+        );
+        setIsVerified(true);
+        setUser(data.user);
+      } catch (error) {
+        console.log("User is not verified");
+        setIsVerified(false);
+        setUser({});
+      }
+    };
+    handleGetUser();
+  }, [isVerified]);
   return (
     <div>
       <Router>
-        <Header/>
+        <Header 
+        setTasks={setTasks}
+        tasks={tasks}
+        setIsVerified={setIsVerified}
+        isVerified={isVerified}
+        />
         <Routes>
-          <Route path='/' element={<Home/>}/>
-          <Route path='/login' element={<Login/>}/>
-          <Route path='/register' element={<Register/>}/>
-          <Route path='/profile' element={<Profile/>}/>
+          <Route path='/' element={<Home 
+          isVerified={isVerified}
+          tasks={tasks}
+          setTasks={setTasks}
+          />}/>
+
+          <Route path='/login' element={<Login 
+          isVerified={isVerified}
+          setIsVerified={setIsVerified}
+          />}/>
+
+          <Route path='/register' element={<Register 
+          isVerified={isVerified}
+          setIsVerified={setIsVerified}
+          />}/>
+
+          <Route path='/profile' element={<Profile 
+          user={user}
+          isVerified={isVerified}
+          />}/>
         </Routes>
+        <Toaster/>
       </Router>
     </div>
   )
--- task-management-client/src/Components/Header.jsx
+++ task-management-client/src/Components/Header.jsx
@@ -1,9 +1,63 @@
-import React from 'react'
+import axios from 'axios';
+import { Button } from 'react-bootstrap';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import toast from 'react-hot-toast';
+import { Link } from 'react-router-dom';
 
-const Header = () => {
+
+
+function Header({setTasks, tasks, setIsVerified, isVerified}) {
+  const handleLogout = async () => {
+    try {
+      const {data} = await axios.get(
+        "http://localhost:4000/api/v1/user/logout",
+        {withCredentials: true}
+      );
+      toast.success(data.message)
+      setIsVerified(false);
+    } catch (error) {
+      toast.error(error.response.data.message);
+    }
+  };
   return (
-    <div>Header</div>
-  )
+    <Navbar expand="lg" className="bg-body-tertiary">
+      <Container>
+        <Navbar.Brand href="#home">Task Manager</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Link to={"/"} className='text-decoration-none d-flex align-items-center link-light'>
+              Home
+              </Link>
+              
+            <NavDropdown title="Filter Tasks" id="basic-nav-dropdown">
+              <NavDropdown.Item >All Tasks</NavDropdown.Item>
+              <NavDropdown.Item >
+                Complete Tasks
+              </NavDropdown.Item>
+              <NavDropdown.Item >Incomplete Tasks</NavDropdown.Item>
+              <NavDropdown.Item >
+                Archived Tasks
+              </NavDropdown.Item>
+            </NavDropdown>
+            <Link to={"/profile"} className='text-decoration-none d-flex align-items-center link-light'>
+              My Profile
+            </Link>  
+            <Button
+              className="bg-transparent border-0"
+              style={{ width: "fit-content" }}
+              onClick={handleLogout}
+            >
+              LOGOUT
+            </Button>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
 }
 
-export default Header
+export default Header;
