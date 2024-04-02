314a81740fc32c1e8185532a7b3b51a94f755ab3
rafidmahin20 <submanmahin@gmail.com>
2024-04-02 16:40:24 +0600

    register component done

--- task-management-client/src/Components/Register.jsx
+++ task-management-client/src/Components/Register.jsx
@@ -1,8 +1,8 @@
 import axios from 'axios';
 import { useState } from 'react'
-import { Container } from 'react-bootstrap';
+import { Button, Container, Form } from 'react-bootstrap';
 import toast from 'react-hot-toast';
-import { Navigate } from 'react-router-dom';
+import { Link, Navigate } from 'react-router-dom';
 
 
 
@@ -50,7 +50,42 @@ const Register = ({isVerified, setIsVerified}) => {
     return <Navigate to={"/"}/>
   }
   return (
-    <Container className="d-flex justify-content-center align-content-center" style={{minHeight: "800px"}}>jhg</Container>
+    <Container className="d-flex justify-content-center align-content-center" style={{minHeight: "800px"}}>
+      <Form onSubmit={handleRegister} className="w-100 p-5 mx-auto">
+      <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Label>Name</Form.Label>
+        <Form.Control type="text" placeholder="Enter your name" value={name} onChange={(e) => setName(e.target.value)}/>
+      </Form.Group>
+
+      <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Label>Email</Form.Label>
+        <Form.Control type="email" placeholder="Enter your email" value={email} onChange={(e) => setEmail(e.target.value)}/>
+      </Form.Group>
+
+      <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Label>Phone</Form.Label>
+        <Form.Control type="phone" placeholder="Enter your phone number" value={phone} onChange={(e) => setPhone(e.target.value)}/>
+      </Form.Group>
+
+      <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Label>Password</Form.Label>
+        <Form.Control type="password" placeholder="Enter your password" value={password} onChange={(e) => setPassword(e.target.value)}/>
+      </Form.Group>
+
+      <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Label>Avatar</Form.Label>
+        <Form.Control type="file" onChange={avatarHandler}/>
+      </Form.Group>
+
+      <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Label>Already Registered? <Link to={"/login"} className="text-decoration-none">Login</Link></Form.Label>
+      </Form.Group>
+      <Button variant="warning" type="submit" className="w-100 fw-bold fs-5 text-light">
+        Register
+      </Button>
+    </Form>  
+      
+    </Container>
   )
 }
 
