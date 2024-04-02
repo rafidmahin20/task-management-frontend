81a4fa96f19cf00c6b1569b05281a175f911865f
rafidmahin20 <submanmahin@gmail.com>
2024-04-02 23:12:44 +0600

    editing home page

--- task-management-client/src/Components/Home.jsx
+++ task-management-client/src/Components/Home.jsx
@@ -1,8 +1,68 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useState } from 'react'
+import { Button, Card } from 'react-bootstrap';
+import toast from 'react-hot-toast';
+import { Navigate } from 'react-router-dom';
 
-const Home = () => {
+
+
+const Home = ({isVerified, tasks, setTasks}) => {
+  const [showCreateModal, setShowCreateModal] = useState(false);
+  const [showUpdateModal, setShowUpdateModal] = useState(false);
+  const [showViewModal, setShowViewModal] = useState(false);
+  const [viewTaskId, setViewTaskId] = useState("");
+  const [updateTaskId, setUpdateTaskId] = useState("");
+
+  const deleteTask = async (id) => {
+    await axios.delete(`http://localhost:4000/api/v1/task/delete/${id}`, {
+      withCredentials: true,
+    }).then((res) => {
+      toast.success((prevTasks) => prevTasks.filter((tasks) => tasks._id !== id));
+    }).catch((error) => {
+      toast.error(error.response.data.message);
+    });
+  };
+
+  const handleCreateModalClose = () => setShowCreateModal(false);
+  const handleUpdateModalClose = () => setShowUpdateModal(false);
+  const handleViewModalClose = () => setShowViewModal(false);
+
+  const handleCreateModalShow = () => setShowCreateModal(true);
+
+  const handleUpdateModalShow = (id) => {
+    setUpdateTaskId(id);
+    setShowUpdateModal(true);
+  };
+
+  const handleViewModalShow = (id) => {
+    setViewTaskId(id);
+    setShowViewModal(true);
+  };
+
+  if(!isVerified) {
+    return <Navigate to={"/login"}/>
+  }
   return (
-    <div>Home</div>
+    <div className="container my-4">
+      <div className="row mb-3">
+        <div className="col text-end">
+          <Button variant='primary' onClick={handleCreateModalShow}>Create Task</Button>
+        </div>
+      </div>
+      <div className='row'>
+        {
+          tasks && tasks.length > 0 ? (tasks.map(task => {
+            return(
+              <div key={task._id} className="col-lg-3 col-md-4 col-sm-6">
+                <Card style={{marginBottom: "20px", minHeight: "400px"}}>
+                    
+                </Card>
+              </div>
+            )
+          })) : <h1>You dont have any tasks</h1>
+        }
+      </div>
+    </div>
   )
 }
 
