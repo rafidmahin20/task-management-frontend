56066d9723fcec437617c408b1e3bc206e3d2b15
rafidmahin20 <submanmahin@gmail.com>
2024-04-03 16:07:36 +0600

    profile component done

--- task-management-client/src/Components/Profile.jsx
+++ task-management-client/src/Components/Profile.jsx
@@ -1,11 +1,46 @@
-import React from 'react'
+import React from "react";
+import { Container, Stack } from "react-bootstrap";
+import { Navigate } from "react-router-dom";
 
-
-
-const Profile = () => {
+const Profile = ({ user, isVerified }) => {
+  if (!isVerified) {
+    return <Navigate to={"/login"} />;
+  }
   return (
-    <div>Profile</div>
-  )
-}
+    <>
+      <Container className="my-4">
+        <h1 className="mb-3">PROFILE</h1>
+        {user && (
+          <Stack style={{ width: "fit-content", margin: "0 auto" }} gap={1}>
+            <Stack direction="horizontal" gap={3}>
+              <img
+                style={{
+                  width: "250px",
+                  height: "250px",
+                  borderRadius: "100%",
+                  marginBottom: "100px",
+                }}
+                src={user.avatar && user.avatar.url}
+                alt="avatar"
+              />
+            </Stack>
+            <Stack direction="horizontal" gap={3}>
+              <p className="fw-bold">NAME:</p>
+              <p>{user.name}</p>
+            </Stack>
+            <Stack direction="horizontal" gap={3}>
+              <p className="fw-bold">EMAIL:</p>
+              <p>{user.email}</p>
+            </Stack>
+            <Stack direction="horizontal" gap={3}>
+              <p className="fw-bold">PHONE:</p>
+              <p>{user.phone}</p>
+            </Stack>
+          </Stack>
+        )}
+      </Container>
+    </>
+  );
+};
 
-export default Profile
+export default Profile;
